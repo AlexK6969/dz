570674b7883beeed26c7ce361765866a35e2c1e0
Alexey Kozlov <lexa25.kozlov@yandex.ru>
2023-02-26 21:52:54 +0400

    dz

---
+++ homework1/Program.cs
@@ -0,0 +1,97 @@
+﻿// int a = 5;
+// int b = 7;
+// int max = a;
+// int min = a;
+
+// if(a > max) max=a;
+// if(b > max) max=b;
+// if(a < max) min=a;
+// if(b < max) min=b; 
+// Console.Write("max = ");
+// Console.WriteLine(max);
+// Console.Write("min = ");
+// Console.WriteLine(min);
+
+// int a = 10;
+// int b = 2;
+// int max = a;
+// int min = a;
+
+// if(a > max) max=a;
+// if(b > max) max=b;
+// if(a < max) min=a;
+// if(b < max) min=b; 
+// Console.Write("max = ");
+// Console.WriteLine(max);
+// Console.Write("min = ");
+// Console.WriteLine(min);
+
+// int a = -9;
+// int b = -3;
+// int max = a;
+// int min = a;
+
+// if(a > max) max=a;
+// if(b > max) max=b;
+// if(a < max) min=a;
+// if(b < max) min=b; 
+// Console.Write("max = ");
+// Console.WriteLine(max);
+// Console.Write("min = ");
+// Console.WriteLine(min);
+
+// int a = 2;
+// int b = 3;
+// int c = 7;
+// int max = a;
+
+// if(a > max) max=a;
+// if(b > max) max=b;
+// if(c > max) max=c;
+
+// Console.Write("max ");
+// Console.WriteLine(max);
+
+// int a = 44;
+// int b = 5;
+// int c = 78;
+// int max = a;
+
+// if(a > max) max=a;
+// if(b > max) max=b;
+// if(c > max) max=c;
+
+// Console.Write("max ");
+// Console.WriteLine(max);
+
+// int a = 22;
+// int b = 3;
+// int c = 9;
+// int max = a;
+
+// if(a > max) max=a;
+// if(b > max) max=b;
+// if(c > max) max=c;
+
+// Console.Write("max ");
+// Console.WriteLine(max);
+
+// Console.WriteLine("input number : ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a % 2 == 0) 
+// Console.WriteLine("even");
+// else Console.WriteLine("uneven");
+
+// Console.WriteLine("number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// Console.WriteLine("even numbers of 1 to" + num);
+// while(i <= num)
+// {
+//     if(i % 2 == 0)
+//     {
+//         Console.Write(i + ",");
+//     }
+//     i++;
+// }
